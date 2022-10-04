c3b58bb9d79a9b167d9c4ab120a763a5f15fe3c1
Rifat <reaget.acc@gmail.com>
2022-10-04 08:08:35 +0300

    MRR 2022.10.04 | added random color and some new functionallity

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ class Board {
       [0, 1],
       [1, 1]
     ];
+    this.color = getRandColor();
+    this.bgColor = getRandColor();
+
+    document.body.style['background-color'] = this.bgColor;
   }
 
   createBoard() {
@@ -60,10 +64,16 @@ class Board {
   }
 
   getNeighbour(x, y) {
-    if (x < 0 || y < 0 || x > this.grid.length - 1 || y > this.grid.length - 1)
-      return null;
+    let _x = x, _y = y;
+    if (x < 0) _x = this.grid.length - 1;
+    if (x > this.grid.length - 1) _x = 0;
+    if (y < 0) _y = this.grid.length - 1;
+    if (y > this.grid.length - 1) _y = 0;
 
-    return this.grid[y][x];
+    // if (x < 0 || y < 0 || x > this.grid.length - 1 || y > this.grid.length - 1)
+    //   return null;
+
+    return this.grid[_y][_x];
   }
 
   getNewValue(x, y) {
@@ -91,27 +101,44 @@ class Board {
   render(ctx, size) {
     for (let y = 0; y < this.size; y++) {
       for (let x = 0; x < this.size; x++) {
-        ctx.fillStyle = this.grid[y][x] ? '#000' : '#fff';
+        ctx.fillStyle = this.grid[y][x] ? this.color : this.bgColor;
         ctx.fillRect(x * size, y * size, size, size);
       }
     }
   }
 }
 
-const SIZE = 50;
+function getRandColor() {
+  const r = getRandBetween(0, 255);
+  const g = getRandBetween(0, 255);
+  const b = getRandBetween(0, 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function getRandBetween(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function getCanvasSize() {
+  return window.innerHeight > window.innerWidth ?
+    window.innerWidth : window.innerHeight;
+}
+
+const SIZE = 100;
 const board = new Board(SIZE);
 board.createBoard();
 
 const canvas = document.getElementById('c');
 const ctx = canvas.getContext('2d');
-const W = canvas.width;
-const H = canvas.height;
+const W = canvas.width = getCanvasSize();
+const H = canvas.height = getCanvasSize();
 const cellSize = W / SIZE;
 // board.render(ctx, cellSize);
 // board.update();
 
 setInterval(() => {
-  ctx.fillStyle = '#fff';
+  // ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, W, H);
   board.update();
   board.render(ctx, cellSize);
